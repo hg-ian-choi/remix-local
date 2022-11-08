57a7432ea7f80ee956b655987854250b0a9b9fb2
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-08 20:59:33 +0900

    clone-transparent: try cloning modified transparent proxy contract

--- clone-transparent/contracts/TestFactory.sol
+++ clone-transparent/contracts/TestFactory.sol
@@ -1,37 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./ITest.sol";
+import "./ITestTransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestFactory {
+contract TestFactory is Ownable {
     using Clones for address;
 
-    address public owner;
     address public origin;
 
     event NewClone(address _newClone, address _owner);
 
-    modifier onlyOwner() {
-        require(owner == msg.sender, "ERROR: Only Owner");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-    }
+    constructor() {}
 
     function setOrigin(address _origin) public onlyOwner {
         origin = _origin;
     }
 
-    function _clone(string memory _name, string memory _symbol) external returns (address newAddress) {
-        newAddress = origin.cloneDeterministic(_getSalt(msg.sender, keccak256(abi.encode(_name, _symbol))));
-        ITest(newAddress).initialize(payable(msg.sender), _name, _symbol);
+    function _clone(
+        address _logic,
+        address admin_,
+        bytes memory _data,
+        address payable _owner,
+        string memory _name,
+        string memory _symbol
+    ) external returns (address newAddress) {
+        newAddress = origin.cloneDeterministic(
+            _getSalt(msg.sender, keccak256(abi.encode(_name, _symbol)))
+        );
+        ITestTransparentUpgradeableProxy(newAddress).initialize(_logic, admin_, _data, payable(_msgSender()), _name, _symbol);
         emit NewClone(newAddress, msg.sender);
     }
 
-    function _getSalt(address _creator, bytes32 _nonce) public pure returns (bytes32) {
+    function _getSalt(address _creator, bytes32 _nonce)
+        public
+        pure
+        returns (bytes32)
+    {
         return bytes32(uint256(uint160(_creator))) | _nonce;
     }
 }
