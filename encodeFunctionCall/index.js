c9126fa3bfaf5a0c805bb6782ec981e4c8c52e7f
hg-ian-choi <hg.ian.choi@gmail.com>
2023-03-06 23:35:45 +0900

    erc721uogradeable: add project

--- encodeFunctionCall/index.js
+++ encodeFunctionCall/index.js
@@ -1,27 +1,32 @@
 const Web3 = require('web3');
 
-const web3 = new Web3("https://mainnet.infura.io/v3/dc4f7ecbab794372b17b8be7a4564233");
+const web3 = new Web3(
+  'https://mainnet.infura.io/v3/'
+);
 
 const main = async () => {
-  const result = await web3.eth.abi.encodeFunctionCall({
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "_owner",
-        "type": "address"
-      },
-      {
-        "internalType": "string",
-        "name": "_name",
-        "type": "string"
-      }
-    ],
-    "name": "initialize",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }, ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "b"]);
+  const result = await web3.eth.abi.encodeFunctionCall(
+    {
+      inputs: [
+        {
+          internalType: 'string',
+          name: '_name',
+          type: 'string',
+        },
+        {
+          internalType: 'string',
+          name: '_symbol',
+          type: 'string',
+        },
+      ],
+      name: 'initialize',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    ['TEST', 'test']
+  );
   console.log('result: ', result);
-}
+};
 
 main();
